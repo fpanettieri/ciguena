d82e74655e458470c861a481e3d9123780c77420
Fabio Panettieri <fpanettieri@gmail.com>
2018-12-24 23:34:41 -0300

    file parsed

--- parse.js
+++ parse.js
@@ -1,3 +1,33 @@
-const parse = require('csv-parse');
+'use strict';
 
-const parser = parse({delimiter: ','});
+const csv = require('csv-parser');
+const fs = require('fs');
+const logger = require('./logger');
+
+const log = new logger(`[ciguena/parse]`);
+const output_dir = 'out';
+
+
+// const results = [];
+
+log.info("BEGIN");
+
+log.log('creating output folder');
+if (!fs.existsSync(output_dir)){
+  fs.mkdirSync(output_dir);
+}
+
+return;
+
+fs.createReadStream('gpsfarma.csv')
+  .pipe(csv())
+  .on('data', (data) => {
+    // results.push(data);
+
+
+    // create folder
+    // download image
+  })
+  .on('end', () => {
+    log.info("END");
+  });
