41527ab3c035bf55726840ef3c5b1b54b054eb36
Fabio Panettieri <fpanettieri@gmail.com>
2018-12-25 00:30:25 -0300

    parse file working

--- parse.js
+++ parse.js
@@ -1,13 +1,15 @@
 'use strict';
 
+const https = require('https');
 const csv = require('csv-parser');
 const fs = require('fs');
+
 const logger = require('./logger');
 
 const log = new logger(`[ciguena/parse]`);
 const output_dir = './out';
+const results = [];
 
-// const results = [];
 log.info("BEGIN");
 
 function mkdir(dir) {
@@ -19,23 +21,35 @@ function mkdir(dir) {
   }
 }
 
-mkdir(output_dir);
+function download () {
+  const data = results.pop();
+  if (!data) { return; }
+  log.log(`downloading ${data['Url-Imagen']}`);
 
+  let dir = data['Productos-Href'];
+  dir = dir.replace('https://www.gpsfarma.com/', '');
+  dir = dir.replace(/(\.html)$/, '');
+  dir = dir.replace(/\/$/, '');
+  dir = dir.replace(/-/g, '_');
+  mkdir(`${output_dir}/${dir}`);
+
+  const file_name = `${output_dir}/${dir}/${data['SKU']}`;
+  const file = fs.createWriteStream(file_name);
+
+  log.log(`downloading ${data['Url-Imagen']}`);
+  const request = https.get(data['Url-Imagen'], function(response) {
+    response.pipe(file);
+    file.on('finish', () => {
+      log.log(`file downloaded: ${file_name}`);
+      setTimeout(download, 50);
+    });
+  }).on('error', function(err) {
+    log.error(`download error: ${file_name}`);
+  });
+}
+
+mkdir(output_dir);
 fs.createReadStream('gpsfarma.csv')
   .pipe(csv())
-  .on('data', (data) => {
-    // results.push(data);
-
-    let dir = data['Productos-Href'];
-    dir = dir.replace('https://www.gpsfarma.com/', '');
-    dir = dir.replace(/(\.html)$/, '');
-    dir = dir.replace(/\/$/, '');
-    dir = dir.replace(/-/g, '_');
-    mkdir(`${output_dir}/${dir}`);
-
-    // download image
-    // rename img
-  })
-  .on('end', () => {
-    log.info("END");
-  });
+  .on('data', (data) => results.push(data))
+  .on('end', download);
